c4789238acb39f376c2f7612694776d502d5b166
Paul Maliakel <paulbalu93@gmail.com>
2020-12-18 19:43:23 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const server = express()
+require('dotenv').config();
 
 server.listen(process.env.PORT, () => {
 console.log('running on port '+ process.env.PORT)}
-)
+);
+
